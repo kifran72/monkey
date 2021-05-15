4dd6870a9c2fff4e692ac8b9949ad095544bf641
kifran <rizzon.charles@gmail.com>
2021-05-15 22:52:37 +0200

    save

---
+++ src/Monkeys_assets/public/main.js
@@ -0,0 +1,40 @@
+import monkey from 'ic:canisters/Monkeys';
+import monkey_assets from 'ic:canisters/Monkeys_assets';
+import $ from "jquery";
+import "./index.css";
+import {
+  homePage,
+} from "./templates";
+import { injectHtml } from "./utils";
+
+window.$ = window.jQuery = $;
+
+monkey_assets
+  .retrieve("index.html")
+  .then(injectHtml)
+  .then(() =>
+    $(document).ready(function () {
+  
+      function renderHome() {
+        (async function () {
+          try {
+            $(".home").html(homePage(data)).show();
+          } catch (err) {
+            console.error(err);
+          }
+        })();
+      }
+
+      renderHome();
+
+      const showHome = (index) => {
+        const home = state.results[index];
+        renderHome(homePAge.id);
+      };
+
+
+      window.actions = {
+        showHome,
+      };
+    })
+  );
--- src/monkey_assets/public/main.js
+++
@@ -1,5 +0,0 @@
-import monkey from 'ic:canisters/monkey';
-
-monkey.greet(window.prompt("Enter your name:")).then(greeting => {
-  window.alert(greeting);
-});
