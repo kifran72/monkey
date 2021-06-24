a86080054b87a85d122478264a72c73c8e03a3f3
Kifran <charles@winwez.com>
2021-06-25 00:41:59 +0200

    Add Session for login Metamask

--- src/monkey_assets/src/components/routing/provideAuth.jsx
+++ src/monkey_assets/src/components/routing/provideAuth.jsx
@@ -1,21 +1,61 @@
-import React, { useContext, createContext, useState } from "react";
+import React, { useContext, createContext, useState, setState } from "react";
 import { Route, Redirect } from "react-router-dom";
 import FakeAuth from "./fakeAuth";
+import detectEthereumProvider from "@metamask/detect-provider";
+import Session from 'react-session-api'
+Session.config(true, 0)
+const ethereum = window.ethereum;
 
 const authContext = createContext();
 
+ethereum.on('accountsChanged', (accounts) => {
+    if (accounts[0] === undefined) {
+        Session.clear();
+        window.location.reload();
+        console.log('ici accountsClear')
+    } else {
+        Session.set("user", accounts[0])
+        window.location.reload();
+        console.log('ici accountsChanged')
+    }
+});
+
+
+ethereum.on('chainChanged', (chainId) => {
+    Session.set("user", chainId)
+    window.location.reload();
+});
+
+ethereum.on('disconnect', (user) => {
+    Session.clear();
+    console.log('ici disconnect')
+    // window.location.reload();
+});
+
+// let test = ethereum.isConnected();
+
+// if (!test) {
+//     Session.clear();
+//     console.log('notConnected')
+// }
+
+
 const useProvideAuth = () => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(Session.get("user"));
     console.log(user)
-    const signin = cb => {
+    const signin = async (cb) => {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
         return FakeAuth.signin(() => {
-            setUser("user");
+            Session.set("user", account);
+            setUser(account);
             cb();
         });
     };
 
     const signout = cb => {
         return FakeAuth.signout(() => {
+            Session.clear();
             setUser(null);
             cb();
         });
